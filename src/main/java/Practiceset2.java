d2d764080209052c97df3d330b7ee87358788496
unratedpupet <zphillips.de@gmail.com>
2023-04-23 13:30:49 -0400

    Finished. -Created/Initialized intArray -import Random to make it easy -random int between -10 - 20 -assign array with random int -iterate through array to get average

--- src/main/java/Practiceset2.java
+++ src/main/java/Practiceset2.java
@@ -1,27 +1,37 @@
+import java.util.Random;
+
 public class Practiceset2 {
   public static void main(String[] args) {
     // Your code goes here
-    int[][][] intArray = new int[5][2][4];
-    
-	intArray[0][0][0] = 1;
-    intArray[2][0][2] = 1;
-    intArray[0][1][0] = -1;
-    intArray[4][0][0] = 1;
-    intArray[1][1][3] = 1;
-    intArray[3][0][2] = -1;
-    intArray[2][1][0] = 1;
-    intArray[1][0][3] = 1;
-    intArray[4][0][1] = 1;
-    intArray[2][1][0] = -1;
+    int[][] intArray = new int[3][];
+    int sum = 0;
+    int count = 0;
+
+    intArray[0] = new int[5];
+    intArray[1] = new int[3];
+    intArray[2] = new int[4];
 
 
-    for (int[][] ints : intArray) {
-      for (int j = 0; j < intArray.length; j++) {
-        for (int k = 0; k < intArray.length; k++) {
-          System.out.println(ints[j][k]);
+    Random rand = new Random();
+
+    for (int i = 0; i < intArray.length; i++) {
+      for (int j = 0; j < intArray[i].length; j++) {
+        intArray[i][j] = rand.nextInt(30 + 10) - 10;
+      }
+    }
+
+    for (int i = 0; i < intArray.length; i++) {
+      for (int j = 0; j < intArray[i].length; j++) {
+        if (intArray[i][j] > 0) {
+          sum += intArray[i][j];
+          count++;
         }
       }
     }
+
+    System.out.println("The average is " + sum/count);
+
+
     // Do not modify this class otherwise
   }
 }
