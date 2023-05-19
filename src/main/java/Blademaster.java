dbb147079307a9307b9539503d65a1ac39e08a11
unratedpupet <zphillips.de@gmail.com>
2023-05-19 11:47:35 -0400

    Added new unit, ScratchUnit. -Has ability to 'jump' around board, but can only spawn and recruit adjacent squares.

--- src/main/java/Blademaster.java
+++ src/main/java/Blademaster.java
@@ -7,13 +7,17 @@ public class Blademaster extends Attacker{
     // Battlefocus is going to increase the attack modifier.
     private boolean battleFocus;
 
-    public Blademaster(char symbol, String name, String teamColor, double health, double healthModifier, double damage, double damageModifier, int luck, int xCor, int yCor, int movement, int movementModifier, int numAttacks, boolean homingRocket, boolean offerCheese, boolean hiding) {
-        super(symbol, name, teamColor, health, healthModifier, damage, damageModifier, luck, xCor, yCor, movement, movementModifier, numAttacks);
+    public Blademaster(char symbol, String name, String teamColor, double health, double healthModifier,
+                       double damage, double damageModifier, int luck, int xCor, int yCor, int movement,
+                       int movementModifier, int numAttacks, boolean battleFocus) {
+        super(symbol, name, teamColor, health, healthModifier, damage, damageModifier, luck,
+                xCor, yCor, movement, movementModifier, numAttacks);
         this.battleFocus = battleFocus;
     }
 
     public Blademaster(boolean battleFocus) {
-        super('M', "Battlemaster", "Red", 100.0, 0.0, 25.0, 0.0, 0, 5, 5, 1, 0, 0);
+        super('M', "Battlemaster", "Red", 100.0, 0.0,
+                25.0, 0.0, 0, 5, 5, 1, 0, 0);
         this.battleFocus = battleFocus;
     }
 
@@ -30,7 +34,9 @@ public class Blademaster extends Attacker{
 
     @Override
     public Blademaster spawn() {
-        Blademaster bm = new Blademaster('m', "Blademaster", "Blu", 100.0, 5.0, 25.0, 10.0, 0, 1, 1, 1, 1, 0, true, true, false);
+        Blademaster bm = new Blademaster('m', "Blademaster", "Blu",
+                100.0, 5.0, 25.0, 10.0,
+                0, 1, 1, 1, 1, 0, false);
         return bm;
     }
 
