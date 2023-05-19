d831b3eef4acf8e53fa073e45faaea8de7799cc7
Leo Chen <leochen533@gmail.com>
2023-05-18 22:27:12 -0400

    Spacing I think?

--- src/main/java/BartSimpsonUnit.java
+++ src/main/java/BartSimpsonUnit.java
@@ -6,6 +6,7 @@ import static java.lang.Math.abs;
  * <h3>University of Delaware</h3>
  * Practice with classes, constructors, getters and setters
  * with American classic The Simpsons
+ * 
  * @author Zach Phillips
  * @version 1.2
  * @since 2023-02-14
@@ -13,136 +14,140 @@ import static java.lang.Math.abs;
 
 public class BartSimpsonUnit extends Recruiter {
 
-    private int numTimesSpawned;
-    private boolean distract;
-    private boolean recruit;
-    final int MAX_NUM_SPAWNED = 2;
+  private int numTimesSpawned;
+  private boolean distract;
+  private boolean recruit;
+  final int MAX_NUM_SPAWNED = 2;
 
+  public BartSimpsonUnit(char symbol, String name, String teamColor, double health, double healthModifier,
+      double damage, double damageModifier, int luck, int xCor, int yCor, int movement, int movementModifier,
+      int numRecruits, int numTimesSpawned, boolean distract, boolean recruit) {
+    super(symbol, name, teamColor, health, healthModifier, damage, damageModifier, luck, xCor, yCor, movement,
+        movementModifier, numRecruits);
+    this.numTimesSpawned = numTimesSpawned;
+    this.distract = distract;
+    this.recruit = recruit;
+  }
 
-    public BartSimpsonUnit(char symbol, String name, String teamColor, double health, double healthModifier, double damage, double damageModifier, int luck, int xCor, int yCor, int movement, int movementModifier, int numRecruits, int numTimesSpawned, boolean distract, boolean recruit) {
-        super(symbol, name, teamColor, health, healthModifier, damage, damageModifier, luck, xCor, yCor, movement, movementModifier, numRecruits);
-        this.numTimesSpawned = numTimesSpawned;
-        this.distract = distract;
-        this.recruit = recruit;
+  public BartSimpsonUnit(int numTimesSpawned, boolean distract, boolean recruit) {
+    super('B', "Bart Simpson", "Red", 100.0, 0.0, 25.0, 0.0, 0, 5, 5, 1, 0, 0);
+    this.numTimesSpawned = numTimesSpawned;
+    this.distract = distract;
+    this.recruit = recruit;
+  }
+
+  public BartSimpsonUnit() {
+    this(0, true, true);
+  }
+
+  public int getNumTimesSpawned() {
+    return numTimesSpawned;
+  }
+
+  public void setNumTimesSpawned(int numTimesSpawned) {
+    this.numTimesSpawned = numTimesSpawned;
+  }
+
+  public boolean canDistract() {
+    return distract;
+  }
+
+  public void setDistract(boolean distract) {
+    this.distract = distract;
+  }
+
+  public boolean canRecruit() {
+    return recruit;
+  }
+
+  public void setRecruit(boolean recruit) {
+    this.recruit = recruit;
+  }
+
+  public int getMAX_NUM_SPAWNED() {
+    return MAX_NUM_SPAWNED;
+  }
+
+  public void takeDamage(double damage) {
+    this.health -= damage;
+  }
+
+  /**
+   * Checks to see if the unit can be distracted, and prints out that it is
+   * distracted.
+   */
+  public void distracted() {
+    if (this.canDistract()) {
+      System.out.println("Unit Distracted");
+    }
+  }
+
+  /**
+   * canSpawn – this method has no parameters and will return true or false
+   * based on whether the piece can make a copy of itself
+   * only original pieces can make copies
+   * pieces can only spawn if it hasn’t reached max number of times it can spawn
+   *
+   */
+  public boolean canSpawn() {
+    if (this.symbol == 'B') {
+      if (numTimesSpawned < MAX_NUM_SPAWNED) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Spawns a new Bart Simpson after checking to see if it is spawnable.
+   * 
+   * @return A new Bart Simpson Unit.
+   */
+  public BartSimpsonUnit spawn() {
+    if (this.canSpawn()) {
+      numTimesSpawned++;
+      return new BartSimpsonUnit('b', "Bart Simpson", "Blue", 100.0, 5.0, 25.0, 10.0, 0, 1, 1, 1, 1, 0, 0, true, true);
     }
 
-    public BartSimpsonUnit(int numTimesSpawned, boolean distract, boolean recruit) {
-        super('B', "Bart Simpson", "Red", 100.0, 0.0, 25.0, 0.0, 0, 5, 5, 1, 0, 0);
-        this.numTimesSpawned = numTimesSpawned;
-        this.distract = distract;
-        this.recruit = recruit;
+    return null;
+  }
+
+  public boolean validSpawnPath(int fromRowIndex, int fromColIndex, int toRowIndex, int toColIndex) {
+
+    // Up and down the column, but no movement across the row.
+    if (fromColIndex == toColIndex && fromRowIndex != toRowIndex) {
+      return true;
     }
-
-    public BartSimpsonUnit() {this(0, true, true);}
-
-
-    public int getNumTimesSpawned() {
-        return numTimesSpawned;
+    // Only two spaces across the row, no movement up or down column.
+    else if ((abs(fromColIndex - toColIndex) <= 2) && fromRowIndex == toRowIndex) {
+      return true;
     }
+    return false;
+  }
 
-    public void setNumTimesSpawned(int numTimesSpawned) {
-        this.numTimesSpawned = numTimesSpawned;
+  public boolean validMovePath(int fromRowIndex, int fromColIndex, int toRowIndex, int toColIndex) {
+
+    // Up and down the column, but no movement across the row.
+    if (fromColIndex == toColIndex && fromRowIndex != toRowIndex) {
+      return true;
     }
-
-    public boolean canDistract() {
-        return distract;
+    // Only two spaces across the row, no movement up or down column.
+    else if ((abs(fromColIndex - toColIndex) <= 2) && fromRowIndex == toRowIndex) {
+      return true;
     }
+    return false;
+  }
 
-    public void setDistract(boolean distract) {
-        this.distract = distract;
+  public boolean validRecruitPath(int fromRowIndex, int fromColIndex, int toRowIndex, int toColIndex) {
+
+    // Up and down the column, but no movement across the row.
+    if (fromColIndex == toColIndex && fromRowIndex != toRowIndex) {
+      return true;
     }
-
-    public boolean canRecruit() {
-        return recruit;
+    // Only two spaces across the row, no movement up or down column.
+    else if ((abs(fromColIndex - toColIndex) <= 2) && fromRowIndex == toRowIndex) {
+      return true;
     }
-
-    public void setRecruit(boolean recruit) {
-        this.recruit = recruit;
-    }
-
-    public int getMAX_NUM_SPAWNED() {
-        return MAX_NUM_SPAWNED;
-    }
-
-    public void takeDamage(double damage) {this.health -= damage; }
-
-    /**
-     * Checks to see if the unit can be distracted, and prints out that it is distracted.
-     */
-    public void distracted() {
-        if (this.canDistract()) {
-            System.out.println("Unit Distracted");
-        }
-    }
-
-    /**
-     * canSpawn – this method has no parameters and will return true or false
-     * based on whether the piece can make a copy of itself
-     * only original pieces can make copies
-     * pieces can only spawn if it hasn’t reached max number of times it  can spawn
-     *
-     */
-    public boolean canSpawn() {
-        if (this.symbol == 'B') {
-            if (numTimesSpawned < MAX_NUM_SPAWNED) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    /**
-     * Spawns a new Bart Simpson after checking to see if it is spawnable.
-     * @return A new Bart Simpson Unit.
-     */
-    public BartSimpsonUnit spawn() {
-        if (this.canSpawn()) {
-            numTimesSpawned++;
-            return new BartSimpsonUnit('b', "Bart Simpson", "Blue", 100.0, 5.0, 25.0, 10.0, 0, 1, 1, 1, 1, 0, 0,true, true);
-        }
-
-        return null;
-    }
-
-
-
-    public boolean validSpawnPath(int fromRowIndex, int fromColIndex, int toRowIndex, int toColIndex) {
-
-        //Up and down the column, but no movement across the row.
-        if (fromColIndex == toColIndex && fromRowIndex != toRowIndex) {
-            return true;
-        }
-        //Only two spaces across the row, no movement up or down column.
-        else if ((abs(fromColIndex - toColIndex) <= 2) && fromRowIndex == toRowIndex) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean validMovePath(int fromRowIndex, int fromColIndex, int toRowIndex, int toColIndex) {
-
-        //Up and down the column, but no movement across the row.
-        if (fromColIndex == toColIndex && fromRowIndex != toRowIndex) {
-            return true;
-        }
-        //Only two spaces across the row, no movement up or down column.
-        else if ((abs(fromColIndex - toColIndex) <= 2) && fromRowIndex == toRowIndex) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean validRecruitPath(int fromRowIndex, int fromColIndex, int toRowIndex, int toColIndex) {
-
-        //Up and down the column, but no movement across the row.
-        if (fromColIndex == toColIndex && fromRowIndex != toRowIndex) {
-            return true;
-        }
-        //Only two spaces across the row, no movement up or down column.
-        else if ((abs(fromColIndex - toColIndex) <= 2) && fromRowIndex == toRowIndex) {
-            return true;
-        }
-        return false;
-    }
+    return false;
+  }
 }
--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -6,6 +6,7 @@ import java.util.Collections;
  * <h2>CISC 181-052L Spring 2023</h2>
  * <h3>University of Delaware</h3>
  * The Controller class is the user input gameplay class for the game.
+ * 
  * @author Zach Phillips and Leo Chen
  * @version 1.0
  * @since 2023-04-14
@@ -13,157 +14,142 @@ import java.util.Collections;
 
 public class Controller {
 
-    private Game game;
-    private TextView textView;
-    private GameEventsLinkedList gell;
+  private Game game;
+  private TextView textView;
+  private GameEventsLinkedList gell;
 
-    /**
-     * This method sets up the game that was supplied by the instructor.
-     * @return - Game - Returns the game to be played.
-     */
-    public Game setUpGameModel(){
-        // Create 4 pieces for team A
-        // Load the pieces in an ArrayList
-        ArrayList<Unit> piecesTeamA = new ArrayList<Unit>();
+  /**
+   * This method sets up the game that was supplied by the instructor.
+   * 
+   * @return - Game - Returns the game to be played.
+   */
+  public Game setUpGameModel() {
+    // Create 4 pieces for team A
+    // Load the pieces in an ArrayList
+    ArrayList<Unit> piecesTeamA = new ArrayList<Unit>();
 
+    BartSimpsonUnit bs = new BartSimpsonUnit();
+    bs.setTeamColor("Blu");
 
-        BartSimpsonUnit bs = new BartSimpsonUnit();
-        bs.setTeamColor("Blu");
+    TomJerryUnit tj = new TomJerryUnit();
+    tj.setTeamColor("Blu");
 
+    piecesTeamA.add(bs);
+    piecesTeamA.add(tj);
 
-        TomJerryUnit tj = new TomJerryUnit();
-        tj.setTeamColor("Blu");
+    // Create a team object
+    Team teamA = new Team("Blu", piecesTeamA);
+    Player playerOne = new Player(1, true, teamA);
 
+    // Create 4 pieces for team B
+    // Load the pieces in an ArrayList
+    ArrayList<Unit> piecesTeamB = new ArrayList<Unit>();
 
-        piecesTeamA.add(bs);
-        piecesTeamA.add(tj);
+    BartSimpsonUnit bs2 = new BartSimpsonUnit();
+    bs2.setTeamColor("Red");
 
+    TomJerryUnit tj2 = new TomJerryUnit();
+    tj2.setTeamColor("Red");
 
-        // Create a team object
-        Team teamA = new Team("Blu",piecesTeamA);
-        Player playerOne = new Player(1, true, teamA);
+    // Create a team object
+    Team teamB = new Team("Red", piecesTeamB);
+    Player playerTwo = new Player(2, false, teamB);
+    teamB.addUnitsToTeam(bs2);
+    teamB.addUnitsToTeam(tj2);
 
+    // Create an instance of the game
+    return new Game(8, 8, playerOne, playerTwo);
+  }
 
-        // Create 4 pieces for team B
-        // Load the pieces in an ArrayList
-        ArrayList<Unit> piecesTeamB = new ArrayList<Unit>();
+  public Controller() {
+    game = setUpGameModel();
+    textView = new TextView();
+    gell = new GameEventsLinkedList();
+    textView.updateView(game);
+  }
 
+  /**
+   * This method creates an instance of either ActionMove,
+   * ActionAttack, ActionRecruit, or ActionSpawn based on which
+   * action type is passed to this method and calls its performAction method.
+   * 
+   * @param fromSquareRow - int index of from square row
+   * @param fromSquareCol - int index of from square column
+   * @param toSquareRow   - int index of to square row
+   * @param toSquareCol   - int index of to square column
+   * @param action        - char representing the action type.
+   */
+  public void carryOutAction(
+      int fromSquareRow, int fromSquareCol, int toSquareRow,
+      int toSquareCol, char action) {
+    GameEvent ge = null;
+    int currentPlayerNum = game.getCurrentPlayer().getPlayerNumber();
+    switch (action) {
+      case 'M':
+        ActionMove am = new ActionMove(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
+        am.performAction();
+        ge = new GameEvent(currentPlayerNum, "Move", am.toString());
+        break;
+      case 'A':
+        ActionAttack aa = new ActionAttack(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
+        aa.performAction();
+        ge = new GameEvent(currentPlayerNum, "Attack", aa.toString());
+        break;
+      case 'R':
+        ActionRecruit ar = new ActionRecruit(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
+        ar.performAction();
+        ge = new GameEvent(currentPlayerNum, "Recruit", ar.toString());
+        break;
+      case 'S':
+        ActionSpawn as = new ActionSpawn(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
+        as.performAction();
+        ge = new GameEvent(currentPlayerNum, "Spawn", as.toString());
+        break;
+    }
+    gell.push(new GameEventNode(ge));
+  }
 
-        BartSimpsonUnit bs2 = new BartSimpsonUnit();
-        bs2.setTeamColor("Red");
+  public void playGame() {
+    // Need a while statement that runs step a and step b until the action is valid
+    // per checkValidAction()
+    boolean validAction = false;
+    while (!validAction) {
 
-
-        TomJerryUnit tj2 = new TomJerryUnit();
-        tj2.setTeamColor("Red");
-
-        // Create a team object
-        Team teamB = new Team("Red",piecesTeamB);
-        Player playerTwo = new Player(2, false, teamB);
-        teamB.addUnitsToTeam(bs2);
-        teamB.addUnitsToTeam(tj2);
-
-        // Create an instance of the game
-        return new Game(8, 8, playerOne, playerTwo);
+      textView.getNextPlayersAction(game);
+      validAction = Rules.checkValidAction(
+          game, textView.getFromRow(), textView.getFromCol(),
+          textView.getToRow(), textView.getToCol(), textView.getAction());
     }
 
-    public Controller() {
-        game = setUpGameModel();
-        textView = new TextView();
-        gell = new GameEventsLinkedList();
-        textView.updateView(game);
+    carryOutAction(
+        textView.getFromRow(), textView.getFromCol(),
+        textView.getToRow(), textView.getToCol(), textView.getAction());
+
+    System.out.println(game);
+
+    // if not isAWinner(), repeat playGame() **recursion??** or while loop?
+    if (!game.isAWinner()) {
+      playGame();
+    } else {
+      System.out.println("Winning move: " + gell.pop().getGameState().getEventDetails());
+      textView.printEndOfGame(game);
+      ArrayList<GameEventsLinkedList> events = new ArrayList<GameEventsLinkedList>();
+
+      while (!gell.isEmpty()) {
+        events.add(gell.pop(gell.getHead().getGameState().getEventType()));
+      }
+
+      Collections.sort(events);
+      for (GameEventsLinkedList event : events) {
+
+        System.out.println(event.getHead().getGameState().getEventType() + " " + event.getSize());
+      }
     }
+  }
 
-    /**
-     * This method creates an instance of either ActionMove,
-     * ActionAttack, ActionRecruit, or ActionSpawn based on which
-     * action type is passed to this method and calls its performAction method.
-     * @param fromSquareRow - int index of from square row
-     * @param fromSquareCol - int index of from square column
-     * @param toSquareRow - int index of to square row
-     * @param toSquareCol - int index of to square column
-     * @param action - char representing the action type.
-     */
-    public void carryOutAction(
-            int fromSquareRow, int fromSquareCol, int toSquareRow,
-            int toSquareCol, char action
-            ) {
-        GameEvent ge = null;
-        int currentPlayerNum = game.getCurrentPlayer().getPlayerNumber();
-        switch (action) {
-            case 'M':
-                ActionMove am = new ActionMove(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
-                am.performAction();
-                ge = new GameEvent(currentPlayerNum, "Move", am.toString());
-                break;
-            case 'A':
-                ActionAttack aa = new ActionAttack(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
-                aa.performAction();
-                ge = new GameEvent(currentPlayerNum, "Attack", aa.toString());
-                break;
-            case 'R':
-                ActionRecruit ar = new ActionRecruit(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
-                ar.performAction();
-                ge = new GameEvent(currentPlayerNum, "Recruit", ar.toString());
-                break;
-            case 'S':
-                ActionSpawn as = new ActionSpawn(game, fromSquareRow, fromSquareCol, toSquareRow, toSquareCol);
-                as.performAction();
-                ge = new GameEvent(currentPlayerNum, "Spawn", as.toString());
-                break;
-        }
-        gell.push(new GameEventNode(ge));
-    }
-
-    public void playGame() {
-        // Need a while statement that runs step a and step b until the action is valid per checkValidAction()
-        boolean validAction = false;
-        while (!validAction) {
-
-            textView.getNextPlayersAction(game);
-            validAction = Rules.checkValidAction(
-                game, textView.getFromRow(), textView.getFromCol(),
-                textView.getToRow(), textView.getToCol(), textView.getAction()
-            );
-        }
-
-        carryOutAction(
-                textView.getFromRow(), textView.getFromCol(),
-                textView.getToRow(), textView.getToCol(), textView.getAction()
-        );
-
-
-        System.out.println(game);
-
-        //if not isAWinner(), repeat playGame() **recursion??** or while loop?
-        if (!game.isAWinner()) {
-            playGame();
-        }
-        else {
-            System.out.println("Winning move: " + gell.pop().getGameState().getEventDetails());
-            textView.printEndOfGame(game);
-            ArrayList<GameEventsLinkedList> events = new ArrayList<GameEventsLinkedList>();
-
-            GameEventsLinkedList Move = gell.pop("Move");
-            GameEventsLinkedList Attack = gell.pop("Attack");
-            GameEventsLinkedList Recruit = gell.pop("Recruit");
-            GameEventsLinkedList Spawn = gell.pop("Spawn");
-
-            events.add(Move);
-            events.add(Attack);
-            events.add(Recruit);
-            events.add(Spawn);
-
-            Collections.sort(events);
-            for (GameEventsLinkedList event: events) {
-                
-                System.out.println(event.getHead().getGameState().getEventType() + " " + event.getSize());
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Controller gamePlay = new Controller();
-        gamePlay.playGame();
-    }
+  public static void main(String[] args) {
+    Controller gamePlay = new Controller();
+    gamePlay.playGame();
+  }
 
 }
